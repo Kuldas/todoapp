2f9c04fc684b94de40f8dbc839f98f0a6fe55af3
Kuldas <jsem@stepankulik.cz>
2021-06-14 02:34:02 +0200

    Title change

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        TODO app by Kulda
+        TODO app by Kuldas
       </h1>
       <h2 class="subtitle">
         My astonishing Nuxt.js project
